368ffbcf1641175b40ef6a2652571e1aabe5eafa
Ubuntu <ubuntu@ip-172-31-36-88.ap-northeast-2.compute.internal>
2021-03-20 11:43:00 +0000

    Sat Mar 20 11:43:00 UTC 2021

---
+++ lawn_farmer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#github uploader
+git add .
+git commit > lawn_farmer.log
+git push origin master > lawn_farmer.log
+
+echo "============================================================" > lawn_farmer.log
+echo " " > lawn_farmer.log
+
